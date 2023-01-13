dbd0521f228367f10ec43d67fdf258c463ce7a2d
Nox-In-Box <zoe.heidt@gmail.com>
2023-01-13 13:31:55 -0500

    we made a procces for vison stuff

--- scripts/yoloLoader.py
+++ scripts/yoloLoader.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import argparse
 import cv2
@@ -9,50 +10,122 @@ import cv2
 #do not look at this code it is very messy
 
 #creating the blob, remember this later
-blob = cv2.dnn.blobFromImage(image, (416, 416), (0, 0, 0), True, crop = False);
+
 
 #accesssing the network, this will be what is referenced later in get_output and draw_box. The network passes in the
 #weights file and the yolo config file. 
-net = cv2.dnn.readNetFromDarknet(args.weights, args.config);
 
 #all files/text that will be passed in when we run the program in the terminal. this should hopefully work but ehh
-argparse = argparse.ArgumentParser();
+ap = argparse.ArgumentParser()
 
-ap.add_argument('-w', '--weights', required = True);
-ap.add_argument('-i', '--image', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/labeledData');#should be the path to our input images
-ap.add_argument('-c', '--config', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/yoloCon-tiny-mk1.cfg');
-ap.add_argument('-cl', '--classes', required = True);
+ap.add_argument('-w', '--weights', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/backup')
+ap.add_argument('-i', '--image', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/labeledData')#should be the path to our input images put it's the folder they're in
+ap.add_argument('-c', '--config', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/yoloCon-tiny-mk1.cfg')
+ap.add_argument('-cl', '--classes', required = True, help = '/home/team1038/repos/2023VisionProject/mk1/labels.txt')
 
+args = ap.parse_args()
 #the image!!!!
-image = cv2.imread(args.image);
+image = cv2.imread(args.image)
+
+Width = image.shape[1]
+Height = image.shape[0]
+
+scale = 1.0 / 255 # this is the default you don't need to pass this in
+
+#read class names from text files
+
+classes = None
+with open(args.classes, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+#not sure if this needs to be changed
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+net = cv2.dnn.readNetFromDarknet(args.config, args.weights)
+
+blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop = False)
 
 #the output!!!!
-net.setInput(blob);
+net.setInput(blob)
 
 
 #unfinished code. see 'output layer and bounding box' section of reference code. will *hopefully* access layers
 #from the network.
-def get_output (net) {
+def get_output (net):
 
     #accessing layer names from network
-    layer_names = net.getLayerNames();
+    layer_names = net.getLayerNames()
+    print(net.getUnconnectedOutLayers())
+    output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
 
-    output_layers;
-
-    return output_layers;
-    }
+    return output_layers
+    
 
 #also very unfinished, relies almost entirely on line 16 correctly accessing the network and the network correctly
 #having stored the layers and colours. ask connor, idk.
 
-def draw_box (image, x, y, x_plus_w, y_plus_h) {
+def draw_bounding_box (img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
-    #will draw a box around objects. [colors] represents the colors that each object the network is trained to
-    #recognize should have. for example cones should be preassigned bright green (i think). 
+    label = str(classes[class_id])
 
-    cv2.rectangle(image, (x,y), (x_plusw, y_plus_h), [COLORS], 1);
+    color = COLORS[class_id]
 
-    #puts text/labels next to the box. for example a cone should say "cone".
+    cv2.rectangle(img, (x,y), (x_plus_w, y_plus_h), color, 2)
 
-    cv2.putText(img, [LABEL], (x-10, y-10), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.5, [COLORS],2); 
-    }
+    cv2.putText(img, label, (x-10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
+outs = net.forward(get_output(net))
+
+class_ids = []
+
+confidences = []
+
+boxes = []
+
+conf_threshold = 0.3
+nms_threshold = 0.2
+
+for out in outs:
+    for detection in out:
+
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        
+        if confidence > conf_threshold:
+            
+            center_x = int(detection[0] * Width)
+            center_y = int(detection[1] * Height)
+            w = int(detection[2] * Width)
+            h = int(detection[3] * Height)
+            x = center_x - w/2
+            y = center_y - h/2
+            class_ids.append(class_id)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+            
+
+
+indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+print(indices)
+
+for i in indices:
+
+    box = boxes[i]
+
+    x = box[0]
+
+    y = box[1]
+
+    w = box[2]
+
+    h = box[3]
+
+    draw_bounding_box(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+
+image = cv2.resize(image, (900, 2000))
+
+cv2.imshow('object detection', image)
+
+cv2.waitKey()
