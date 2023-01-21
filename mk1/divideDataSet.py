b70dabca47703a86713150c783d76cd7d4863d9e
Connor2573 <Connor.park2573@gmail.com>
2023-01-21 17:16:28 -0500

    adding scripts to divide up train/test data, loading in yoloCon starting weights and yoloCon tiny mk1 cfg, this will need to be modified for our purposes

---
+++ mk1/divideDataSet.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+pathToRepo = r'D:\FRCLakota\2023VisionProject'
+folderName = 'labeledData\\'
+extension = ".jpg"
+trainingLoc = pathToRepo + r'\mk1\train.txt'
+validationLoc = pathToRepo + r'\mk1\validate.txt'
+imageLocations = pathToRepo + r'\mk1\labeledData'
+edge = .95
+
+if(os.path.exists(trainingLoc)):
+    os.remove(trainingLoc)
+    
+if(os.path.exists(validationLoc)):
+    os.remove(validationLoc)
+
+with open(trainingLoc, 'w', encoding='utf-8') as f:
+    with open(validationLoc, 'w', encoding='utf-8') as f2:
+        for root, dirs, files in os.walk(imageLocations):
+            count = 0
+            random.shuffle(files)
+            for file in files:
+                if file.endswith(extension) and count <= len(files) * edge:
+                    f.write(folderName + file + "\n")
+                elif file.endswith(extension):
+                    f2.write(folderName + file + "\n")
+                count = count + 1
