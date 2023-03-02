374503f6a98062b3f8f3535d1c775834c27a5ab4
Nox Heidt <zoe.heidt@gmail.com>
2023-03-02 09:30:50 -0500

    this is the things that works

--- scripts/CamServ.py
+++ scripts/CamServ.py
@@ -1,61 +1,17 @@
+from flask import Flask, Response
+import cv2
 
-import os, time
-from glob import glob
-import sys
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+app = Flask(__name__)
+cam = cv2.VideoCapture(0)
 
-boundary = '--boundarydonotcross'
+def get_image():
+    while True:
+        ret, img = cam.read()
+        _, frame = cv2.imencode('.jpg', img)
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
 
-def request_headers():
-    return {
-        'Cache-Control': 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0',
-        'Connection': 'close',
-        'Content-Type': 'multipart/x-mixed-replace;boundary=%s' % boundary,
-        'Expires': 'Mo, 3 Jan 2000 12:34:56: GMT',
-        'Pragma': 'no-cache',
-    }
+@app.route('/stream')
+def stream():
+    return Response(get_image(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def image_headers(filename):
-    return {
-        'X-Timestamp': time.time(),
-        'Content-Length': os.pth.getsize(filename),
-        'Content-Type': 'image/jpeg',
-    }
-
-def image(filename):
-    with open(filename, "rb") as f:
-        byte = f.read(1)
-
-        while byte:
-            yield byte
-
-            byte = f.read(1)
-
-
-class MyHandler(BaseHTTPRequestsHandler):
-    def do_GET(self):
-        self.sendresponse(200)
-
-        for k, v in pymjpeg.requests_headers().items():
-            self.send_header(k, v)
-
-
-        for filename in glab('img/*'):
-            self.end_headers()
-            self.wfile.write(pymjpeg.boundary)
-            self.end_headers()
-
-            for k, v in pymjpeg.image_headers(filename).items():
-                self.send_header(k, v)
-            self.end_headers()
-
-            for chunk in pymjpeg.image_headers(filename).items():
-                self.wfile.write(chunk)
-    def log_message(self, format, *args):
-        return
-
-httpd = HTTPServer(('', 8001), MyHandler)
-httpd.serve_forever()
-        
-
-    
+app.run(host='0.0.0.0', port = 1180, threaded=True)
